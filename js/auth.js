f67795b8f2047933d4ed43a9e5e6fc5b42c479a7
stasybashin <anastasiya.polka@gmail.com>
2026-03-13 23:44:33 +0300

    wwwe1

--- js/auth.js
+++ js/auth.js
@@ -442,6 +442,7 @@ function bindProfilePhoneMask() {
 
 document.addEventListener("DOMContentLoaded", () => {
   bindAuthUi();
+  
   bootstrapAuth();
   initCookieBanner();
   bindProfilePhoneMask();
